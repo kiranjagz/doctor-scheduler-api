4586c75a9cc9aab6120b836cfdea6deadbc623ee
Kiran <kiranjagz@gmai.com>
2022-06-12 10:17:10 +0200

    feat: added insert scripts for attendees

---
+++ sql_scripts/insert_script_attendees.sql
@@ -0,0 +1,14 @@
+USE [DoctorSchedulerApiDb]
+GO
+
+INSERT INTO [dbo].[Attendees]([IdNumber],[Name],[Email],[Address])
+     VALUES ('00000000000','Jeff','jefff@email.com', '3 Jeff Place')
+
+INSERT INTO [dbo].[Attendees]([IdNumber],[Name],[Email],[Address])
+     VALUES ('1234567890123','Kiran','kiran@email.com', '1 Road Close')
+
+INSERT INTO [dbo].[Attendees]([IdNumber],[Name],[Email],[Address])
+     VALUES ('3210987654321','Bob','bob@test', '2 Park Avenue')
+GO
+
+
