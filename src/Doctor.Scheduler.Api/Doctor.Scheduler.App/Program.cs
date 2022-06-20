2e7a62ae62a3331df3b46114ecedd92b420f4d61
Kiran <kiranjagz@gmai.com>
2022-06-20 15:18:42 +0200

    feat: added a console app for ef core

---
+++ src/Doctor.Scheduler.Api/Doctor.Scheduler.App/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Doctor.Scheduler.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Read();
+        }
+    }
+}
